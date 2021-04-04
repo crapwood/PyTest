955e0cd5a0b1d651171360660f7f8cf0a0b5fbb8
jbenit3x <johnx.james.benitez@intel.com>
2021-04-04 04:27:05 +0300

    cleanup

--- pytest_exercise/myapp/tests/test_http_method.py
+++ pytest_exercise/myapp/tests/test_http_method.py
@@ -6,42 +6,36 @@ import requests
 
 @pytest.mark.django_db
 class Testing:
+    bot_1 = {
+            "name": "Bot 1",
+            "display_name": "Bot A",
+            "provider": "Client A",
+            "credentials": 1
+        }
+    bot_2 = {
+            "name": "Bot 2",
+            "display_name": "Bot B",
+            "provider": "Client B",
+            "credentials": 1
+        }
 
     def test_url_index(self):
         path = reverse('index')
         assert resolve(path).view_name == 'index'
 
     def test_get_method(self):
-        temp_data = {
-            "name": "Bot 1",
-            "display_name": "Bot A",
-            "provider": "Client A",
-            "credentials": 1
-        }
         url = 'http://127.0.0.1:8000/myapp/'
-        r = requests.post(url, json=temp_data)
+        r = requests.post(url, json=self.bot_1)
         assert Bot.is_bot_in_db(Bot, name='Bot 1') == True
 
     def test_post_method(self):
-        temp_data = {
-            "name": "Bot 2",
-            "display_name": "Bot B",
-            "provider": "Client B",
-            "credentials": 1
-        }
         url = 'http://127.0.0.1:8000/myapp/'
-        r = requests.post(url, json=temp_data)
+        r = requests.post(url, json=self.bot_2)
         assert Bot.is_bot_in_db(Bot, name='Bot 2') == True
 
     def test_put_method(self):
-        temp_data = {
-            "name": "Bot 2",
-            "display_name": "Bot B",
-            "provider": "Client C",
-            "credentials": 1
-        }
         url = 'http://127.0.0.1:8000/myapp/'
-        r = requests.put(url, json=temp_data)
+        r = requests.put(url, json=self.bot_2)
         assert Bot.is_bot_in_db(Bot, name='Bot 2') == True
 
     def test_delete_method(self):
